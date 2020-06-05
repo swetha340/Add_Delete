e5e467ad715f00f3917e599c574a03f836f24e0b
swetha340 <46365410+swetha340@users.noreply.github.com>
2020-06-05 01:49:03 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import Available from './Available';
 
 function App() {
+  const [pos,setPos] = useState(['C','Java','Python','C++','Perl','react']);
+  const [avail,setAvail] = useState(['react']);
+  const addItem =(key)=>{
+    let i;
+      for(i=0;i<avail.length;i++)
+      {
+        if(avail[i]==key)
+        {
+          break;
+        }
+      }
+      if(i==avail.length)
+      {
+         setAvail([...avail,key])
+      }
+  }
+  const deleteItem =(key)=>{
+    const newavail = avail.filter(item=>item!=key);
+    setAvail(newavail);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="row body">
+
+<div className="col-sm-6">
+            <h1>All Positions</h1>
+            <br/><br/>
+
+            {pos.map((item)=>
+            <div key={item}>
+                <div className="row" >
+                  <h3 className="col-sm-4">{item}</h3>
+                  <Button variant="primary" className="col-sm-4" onClick={()=>addItem(item)}>Add</Button>
+                </div>
+                <hr />
+            </div>
+            )}
+
+      </div>
+
+        <Available avail={avail} deleteItem={deleteItem}/>;
+</div>
   );
 }
 
