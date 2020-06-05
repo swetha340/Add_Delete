5babda976135c77c2a9a188ddc38106534cd3765
Pradeep Sunny <19pradeepsunnyc19@gmail.com>
2020-06-05 02:03:37 -0400

    Updates

---
+++ precache-manifest.0dc87ba1e77f4f2ac516820dbc5fe465.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdbff6079b2f65589b036cce47cef52",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "84a96852a745757cec0f",
+    "url": "/todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ec083b23d296e710d339",
+    "url": "/todo/static/css/main.0e51b422.chunk.css"
+  },
+  {
+    "revision": "84a96852a745757cec0f",
+    "url": "/todo/static/js/2.dc5c7385.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo/static/js/2.dc5c7385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec083b23d296e710d339",
+    "url": "/todo/static/js/main.04e25a26.chunk.js"
+  },
+  {
+    "revision": "f82d01502dc99ffea908",
+    "url": "/todo/static/js/runtime-main.3edaac41.js"
+  }
+]);
